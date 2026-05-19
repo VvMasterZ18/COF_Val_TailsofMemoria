41c7fcaf5c2033c4463a40b173e2cd6ce29af3dd
VvMasterZ18 <v.coffin@rns.ecolescreatives.com>
2026-05-19 09:55:18 +0200

    walkinaround

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,36 @@
+using NUnit.Framework;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform playerTransform;
+    public float moveSpeed;
+    public bool isChasing;
+    public float chaseDistance;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isChasing)
+        {
+            if(transform.position.x > playerTransform.position.x)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+                transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+            }
+            if(transform.position.x < playerTransform.position.x)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+                transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+            }
+        }
+
+        else
+        {
+            if(Vector2.Distance(transform.position, playerTransform.position) < chaseDistance)
+            {
+                isChasing = true;
+            }
+        }
+    }
+}
